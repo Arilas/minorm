17b38f29901a99ca77e67bd87d418a7e33dd4a4b
Alex Grand <alex@granted.name>
2016-11-15 18:00:10 +0200

    Fix build

--- test/config/index.js
+++ test/config/index.js
@@ -4,7 +4,7 @@ export default {
     host: process.env.MYSQL_HOST || 'localhost',
     port: process.env.MYSQL_PORT || 3306,
     user: process.env.MYSQL_USER || 'local',
-    password: process.env.MYSQL_PASS || 'freeware',
+    password: process.env.hasOwnProperty('MYSQL_PASS') ? process.env.MYSQL_PASS : 'freeware',
     database: process.env.MYSQL_DB || 'minorm_test'
   }
 }
