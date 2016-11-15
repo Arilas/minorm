71b505aaffe62071be365d5eb2a62accf7466870
Alex Grand <alex@granted.name>
2016-11-15 17:55:44 +0200

    Travis build

--- test/config/index.js
+++ test/config/index.js
@@ -2,6 +2,7 @@
 export default {
   connection: {
     host: process.env.MYSQL_HOST || 'localhost',
+    port: process.env.MYSQL_PORT || 3306,
     user: process.env.MYSQL_USER || 'local',
     password: process.env.MYSQL_PASS || 'freeware',
     database: process.env.MYSQL_DB || 'minorm_test'
