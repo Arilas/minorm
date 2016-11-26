ce85a989b150bc9e5e6ea94d58ec9ac672e82f58
Alex Grand <alex@granted.name>
2016-11-26 06:25:55 +0200

    Fix for flow strange error

--- test/integration/fixtures/createFixtureManager.js
+++ test/integration/fixtures/createFixtureManager.js
@@ -5,7 +5,7 @@ function randomString() {
   return Math.random().toString(36).substring(7)
 }
 
-function range(from, to) {
+function range(from, to): Array<number> {
   return Array.from(new Uint32Array(to - from + 1).map((val, index) => index + from))
 }
 
@@ -13,7 +13,7 @@ export function createFixtureManager(manager: Manager) {
 
   let usersCount = 0
 
-  function createUser() {
+  function createUser(): Promise<Object> {
     return manager.getRepository('users')
       .create({
         login: 'user' + (++usersCount),
@@ -22,11 +22,11 @@ export function createFixtureManager(manager: Manager) {
       .save()
   }
 
-  function createUsers(count: number = 1) {
+  function createUsers(count: number = 1): Promise<Array<Object>> {
     return Promise.all(range(0, count).map(createUser))
   }
 
-  function createPostToUser(id: number) {
+  function createPostToUser(id: number): Promise<Object> {
     return manager.getRepository('posts')
       .create({
         title: 'post ' + randomString(),
@@ -36,21 +36,21 @@ export function createFixtureManager(manager: Manager) {
       .save()
   }
 
-  async function createPost() {
+  async function createPost(): Promise<Object> {
     const user = await createUser()
     return createPostToUser(user.id)
   }
 
-  function createPostsToUser(count: number = 1, id: number) {
+  function createPostsToUser(count: number = 1, id: number): Promise<Array<Object>> {
     return Promise.all(range(0, count).map(createPostsToUser.bind(id)))
   }
 
-  async function createPosts(userCount?: number = 1, postsPerUser?: number = 1) {
+  async function createPosts(userCount?: number = 1, postsPerUser?: number = 1): Promise<Array<Array<Object>>> {
     const users = await createUsers(usersCount)
     return Promise.all(users.map(user => createPostsToUser(postsPerUser, user.id)))
   }
 
-  function createPostComment(postId: number, userId: number) {
+  function createPostComment(postId: number, userId: number): Promise<Object> {
     return manager.getRepository('comments')
       .create({
         creator_id: userId,
@@ -60,12 +60,12 @@ export function createFixtureManager(manager: Manager) {
       .save()
   }
 
-  async function createPostCommentWithUser(postId: number) {
+  async function createPostCommentWithUser(postId: number): Promise<Object> {
     const user = await createUser()
     return createPostComment(postId, user.id)
   }
 
-  async function createPostWithComments(count: number = 1) {
+  async function createPostWithComments(count: number = 1): Promise<Array<Object>> {
     const post = await createPost()
     return Promise.all(range(0, count).map(createPostCommentWithUser.bind(post.id)))
   }
